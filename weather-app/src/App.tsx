395b8fbe22f911dcee357eab513568474f1e7f23
vista.vutrajevic@gmail.com <vista.vutrajevic@gmail.com>
2025-09-23 17:19:08 +0200

    Pages added

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Current from './pages/Current';
+import Weekly from './pages/Weekly';
+import Monthly from './pages/Monthly';
+import Details from './pages/Details';
+
 function App() {
-  return <>Weather App</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/current' element={<Current />} />
+          <Route path='/weekly' element={<Weekly />} />
+          <Route path='/monthly' element={<Monthly />} />
+          <Route path='/details' element={<Details />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
